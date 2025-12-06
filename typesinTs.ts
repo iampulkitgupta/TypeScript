f6267480777a93db48a5de62f8d3b26b16393919
Pulkit Gupta <pulkit.noida@gmail.com>
2025-12-06 21:16:28 +0530

    types and inference

---
+++ typesinTs.ts
@@ -0,0 +1,14 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5
+
+let channelName = "PulkitGupta"
+
+let chaiFlavour: string = "masala chai"
+chaiFlavour = "Ginger Tea"
+
+// chaiFlavour = 12
+
+console.log(chaiFlavour)
+
+let chaiOrder: boolean = true
